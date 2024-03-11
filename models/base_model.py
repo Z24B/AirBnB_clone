3fde7c0724caf3f1cc3401c2e9b0e8faa864a3b6
Z24B <zivha.beharie@gmail.com>
2024-03-10 23:52:14 -0700

    AirBNB Console

--- models/base_model.py
+++ models/base_model.py
@@ -13,13 +13,12 @@ class BaseModel:
         """Initaliaze attributes"""
         if kwargs:
             for key, value in kwargs.items():
-                if key == '__class__':
-                    continue
-                elif key == 'created_at' or key == 'updated_at':
-                    value = datetime.strptime(value, '%Y-%m-%dT%H:%M:%S.%f')
+                if key == 'created_at' or key == 'updated_at':
+                    setattr(self, key, datetime.strptime(value, "%Y-%m-%dT%H:%M:%S.%f"))
+                elif key != '__class__':
                 setattr(self, key, value)
         else:
-            self.id = str(uuid4())
+            self.id = str(uuid.uuid4())
             self.created_at = datetime.now()
             self.updated_at = datetime.now()
             from models import storage
@@ -27,8 +26,8 @@ class BaseModel:
 
     def __str__(self):
         """Override string representation of self"""
-        fmt = self.__class__.__name__
-        return "[{}] ({}) {}".format(fmt, self.id, self.__dict__)
+        return "[{}] ({}) {}".format(
+                self.__class__.__name__, self.id, self.__dict__)
 
     def save(self):
         """Updates last updated variable"""
